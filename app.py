9da332759cf5d552d30cc4e965fd422cc64b9e7d
Ashish Singh <110321646+Ashish2Singh@users.noreply.github.com>
2025-05-21 14:00:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,58 @@
+import json
+import streamlit as st
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import numpy as np
+import google.generativeai as genai
+
+# Accessing the API key from Streamlit secrets
+api_key = st.secrets["APIKEY"]
+
+# Configuring the API key for Gemini AI
+genai.configure(api_key=api_key)
+
+# Loading the trained model 
+model = load_model('Mymodel.h5')
+
+
+# Loading class names from JSON file
+class_names_path = 'class_names.json'
+with open(class_names_path, 'r') as f:
+    class_names = json.load(f)
+
+
+# Streamlit app interface
+st.title('PhytoScan')
+
+# Uploading image
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
+
+if uploaded_file is not None:
+    # Displaying uploaded image
+    st.image(uploaded_file, use_container_width=True)
+
+    # Loading image and preparing it for prediction
+    img = image.load_img(uploaded_file, target_size=(224, 224))  # Adjusting the size according to our model
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)  # Adding batch dimension
+    img_array /= 255.0  # Normalizing the image
+
+    # Predicting the class
+    prediction = model.predict(img_array)
+    predicted_class_index = np.argmax(prediction, axis=1)[0]  # Geting the class with highest probability
+    predicted_class_name = class_names[predicted_class_index]  # Maping index to class name
+
+    # Showing the prediction result
+    st.write(f"Predicted Disease: {predicted_class_name}")
+
+    # Using Gemini AI to generate description and prevention content
+    prompt = f"Write a detailed description and prevention method for the disease {predicted_class_name} in plants."
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    response = model.generate_content(prompt)
+
+    # Showing the generated description and prevention
+    if response.text:
+        st.subheader("Description and Prevention")
+        st.write(response.text)
+    else:
+        st.write("No detailed information available for this disease.")
